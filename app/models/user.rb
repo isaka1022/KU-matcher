e975922fbf58f12b4d6debcb7a3acd86c7bbc38b
isaka1022 <isaka1022@gmail.com>
2019-05-03 14:35:43 +0900

    復旧完了

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
+  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+       
+  def full_profile?
+    family_name? && family_name_kana? && first_name? && first_name_kana? && avatar?
+  end
+end
