83f77b4653ad058b8a4e9ed5ddbd67df95a285e1
isaka1022 <isaka1022@gmail.com>
2019-05-03 22:19:14 +0900

    chat.coffeeのコード書くところまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
-       
+    
+ 
   def full_profile?
     family_name? && family_name_kana? && first_name? && first_name_kana? && avatar?
   end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   resources :companies, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "top#index"
+  mount ActionCable.server => '/cable'
 end
