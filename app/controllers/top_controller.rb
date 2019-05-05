42dc620079326e655ae3feb209c6385b6ec5dea3
isaka1022 <isaka1022@gmail.com>
2019-05-05 09:47:03 +0900

    topの表示機能実装完了

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,15 @@
 class TopController < ApplicationController
     def index
+        @company = Company.find(search_id)
+    end
+
+    private 
+    def search_id
+        if params.permitted?
+            params.require(:company).permit(:id)
+        
+        else
+            1
+        end
     end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable#, :confirmable
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :avatar, content_type:["image/jpg", "image/jpeg", "image/png"]
 
