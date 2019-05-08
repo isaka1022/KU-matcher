db1371ba1991e13ad145e7d090483865ca20de97
isaka1022 <isaka1022@gmail.com>
2019-05-08 13:45:51 +0900

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,13 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery//
-//= require jquery.turbolinks
+//= require jquery
+// require jquery.turbolinks
 //= require rails-ujs
 //= require activestorage
 // require turbolinks
 //= require_tree .
+//= require  bootstrap
 
 //= require twitter/bootstrap
 
